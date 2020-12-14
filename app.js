7d9ad85c942bf2fc102e7f2d0a5db7d0a4ed86e3
Joel Ambu <ambujoel@gmail.com>
2020-12-14 16:30:36 +0100

    create paiement route and pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home');
 var dashboardRouter = require('./routes/dashboard');
 var studentRouter = require('./routes/student');
+var paiementRouter = require('./routes/paiement');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/home', homeRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/student', studentRouter);
+app.use('/paiement', paiementRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
