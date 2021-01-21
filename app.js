00af6e2dbb644ec63ebb3d39c024720db51d4b61
frdrcpeter007 <frdrcpeter@gmail.com>
2021-01-21 01:25:34 +0100

    initialize dotenv

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const session = require('express-session');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
