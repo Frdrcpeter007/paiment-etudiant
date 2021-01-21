5bb26d718519ed89138ded977087abafbdcb6fc8
frdrcpeter007 <frdrcpeter@gmail.com>
2021-01-21 07:25:40 +0100

    Remove menu

--- routes/includes/config.js
+++ routes/includes/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     URL() {
       return {
-        "API": "https://api-payment-student.herokuapp.com/api"
-        // "API": "http://localhost:8966/api"
+        //"API": "https://api-payment-student.herokuapp.com/api"
+         "API": "http://localhost:8966/api"
       }
     }
   }
